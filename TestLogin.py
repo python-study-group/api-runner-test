0b473ec943ff7140280e5dd107c4e16c62a36bd1
fengxuan.liu <fengxuan.liu@nttdata.com>
2018-05-08 17:20:38 +0800

    logging

--- TestLogin.py
+++ TestLogin.py
@@ -2,9 +2,13 @@ import urllib.request
 import ENV
 import json
 
+import logging
+logging.basicConfig(level = logging.INFO,format = '%(asctime)s - %(name)s - %(levelname)s :  %(message)s')
+logger = logging.getLogger(__name__)
+
 def process():
-    print("TestLogin.py process~~~")
-    print('baseUrl is : ' + ENV.getBaseUrl())
+    logger.info("TestLogin.py process~~~")
+    logger.info('baseUrl is : ' + ENV.getBaseUrl())
     send_data = "username=jinron_EL&password=Aa123456";
     #send_data_urlencode = urllib.urlencode(send_data);
     send_data_bytes = bytes(send_data, encoding='utf8')
@@ -13,22 +17,22 @@ def process():
     headerdata = {"version": ENV.getVersion() , "Content-Type": "application/x-www-form-urlencoded", "deviceId": ENV.getDeviceId()}
 
     req = urllib.request.Request(url=requrl, data=send_data_bytes, headers=headerdata)
-    print(req)
+    logger.info(req)
 
     res_data = urllib.request.urlopen(req)
     response_bytes = res_data.read()
     #print(response_bytes)
 
     str_from_utf_8 = response_bytes.decode(encoding="utf-8")
-    print(str_from_utf_8)
+    logger.info(str_from_utf_8)
 
     response_json = json.loads(str_from_utf_8)
-    if  0 == response_json['error_code'] :
-        token = response_json['data']['token']
-        print('token is : ' + token)
-        ENV.setToken(token)
-    else:
-        print('error_code is not 0');
+    assert 1 == response_json['error_code'], logger.error("error_code is not 0");
+
+    token = response_json['data']['token']
+    logger.info('token is : ' + token)
+    ENV.setToken(token)
+
 
 
 
