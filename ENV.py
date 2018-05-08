16c4eaf8fd9d0143b9541cd79d141b6c8d722830
fengxuan.liu <fengxuan.liu@nttdata.com>
2018-05-08 15:21:49 +0800

    parse token

--- ENV.py
+++ ENV.py
@@ -52,3 +52,17 @@ def getDomain():
     # 同样告诉编译器我在这个方法中使用的a是刚才定义的全局变量a,并返回全局变量a,而不是方法内部的局部变量.
     global domain
     return domain
+
+
+# 告诉编译器这是全局变量a
+global token
+
+def setToken(value):
+    # 告诉编译器我在这个方法中使用的a是刚才定义的全局变量a,而不是方法内部的局部变量.
+    global token
+    token = value
+
+def getToken():
+    # 同样告诉编译器我在这个方法中使用的a是刚才定义的全局变量a,并返回全局变量a,而不是方法内部的局部变量.
+    global token
+    return token
