89dc777c9cb1094f0cb6fb802d5e5a098178621f
jinyuanfu <690453971@qq.com>
2018-05-09 17:26:54 +0800

    首页接口测试代码

--- TestAll.py
+++ TestAll.py
@@ -1,6 +1,22 @@
 import TestLogin
 import TestGetUserInfo
+import TestGetIndex
+import TestPassWordIsOverdue
+import TestListTopic
+import TestgetPushsCount
+import TestbindUpushDeviceToken
+import TestgetShowTabTaskCount
+import TestgetIndexNoticeList
+import Testsignin
 
 def processAll():
     TestLogin.process();
     TestGetUserInfo.process();
+    TestGetIndex.process();
+    TestPassWordIsOverdue.process();
+    TestListTopic.process();
+    TestgetPushsCount.process();
+    TestbindUpushDeviceToken.process();
+    TestgetShowTabTaskCount.process();
+    TestgetIndexNoticeList.process();
+    Testsignin.process();
